709258ca2ea73cb08f2c7b013f6dd7f4239e6b1c
lagger8 <sbilal8286@gmail.com>
2025-11-22 19:19:58 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    // Auto-open browser on server start 
-    open: true, 
+
+   base: '/portfolio/',
+  build: {
+    outDir: 'dist',
   }
+
+
+  // when its in your IDE uncomment this and comment the above base line
+  // server: {
+  //   // port: 5173, 
+  //   // Auto-open browser on server start 
+  //   open: true, 
+  // }
 })
+EOF
